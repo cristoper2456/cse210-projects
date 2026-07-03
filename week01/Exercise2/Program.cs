daa2fe2dd48bbd48c1ddddb86ca37fb59c903fa8
cristoper2456 <cristophercastrofer@gmail.com>
2026-07-02 23:14:11 -0400

    changed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Hello! What was your grade? " );
+        int grade = int.Parse(Console.ReadLine());
+        Console.WriteLine(grade);
+        string letter = "";
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+            
+        }
+        
+        else if (grade >= 80)
+        {
+            letter = "B";
+            
+        }
+         else if (grade >= 70)
+        {
+            letter = "C";
+            
+        }
+         else if (grade >= 60)
+        {
+            letter = "D";
+            
+           
+        }
+        else
+        {
+            letter = "F";
+            
+        }
+        
+        Console.WriteLine(letter);
+
+        if (grade >= 70)
+        {
+        
+            Console.WriteLine("congratulations, you passed :)");
+        }
+        else
+        {
+    
+            Console.WriteLine("Good luck next semester :(");
+        }
     }
 }
