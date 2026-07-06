3945ddb3dd50a9da67ed26c0a8a7e8104e6242b9
cristoper2456 <cristophercastrofer@gmail.com>
2026-07-06 19:29:02 -0400

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.Write("What is the magic Number ");
+        int guess = int.Parse(Console.ReadLine());
+        Console.WriteLine(guess);
+
+        string message = "";
+        while (guess != number)
+        {
+            
+            if (guess < number)
+            {
+                message = "a little higher"; 
+                
+                
+            }
+            else if (guess > number)
+            {
+                message = "a little lower";
+                
+            }
+            Console.Write("guess again ");
+            guess = int.Parse(Console.ReadLine());
+            Console.WriteLine(guess);
+            Console.WriteLine(message);
+        }
+    Console.WriteLine("you got it");
+    
     }
 }
